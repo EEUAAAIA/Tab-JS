155d385496513f255a915cc4a1c6ad554b8615ab
EEUAAAIA <eeuaaaia168@outlook.fr>
2022-10-21 14:41:00 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+/*Deux méthodes pour écrire tabelau : new et écriture littérale */
+new nomsChats = new Array("Minou", "John Snow", "Sia");
+
+var nomsChiens = ["Wolf", "Pipeau", "Jox", "Jeepsee", "Princesse"];
+
+/*Pour remplir un tableau*/
+var monTableau = [];
+monTableau[0] = "ballon jaune";
+monTableau[1] = "ballon bleu";
+monTableau[2] = "ballon rouge";
+
+var item = [];
+item[0] = "pomme";
+item[1] = 4+8;
+item[2] = 2;
+item[3] = item[2] + item[1];
+
+
+var nomDino = [];
+nomDino[0] = "Titi";
+nomDino[1] = "Rara";
+nomDino[2] = "Tritri";
+
+/*Tableaux multidimensionnels : tab dans tab*/ 
+var meilleur_combattant = [];
+meilleur_combattant[0] = "Muay Thai";
+meilleur_combattant[0][0] = "Saenchai";
+meilleur_combattant[0][1] = "Buakaw";
+meilleur_combattant[0][2] = "Saiyok";
+meilleur_combattant[0][3] = "Bouganhem";
+meilleur_combattant[0][4] = "Meksen";
+meilleur_combattant[1] = "Boxe anglaise";
+meilleur_combattant[1][0] = "Tyson";
+meilleur_combattant[1][1] = "Lomanchenko";
+meilleur_combattant[1][2] = "Ray Leonard";
+meilleur_combattant[1][3] = "Ali";
+meilleur_combattant[1][4] = "Mayweather";
+meilleur_combattant[2] = "Acting";
+meilleur_combattant[2][0] = "Kung-Fu";
+meilleur_combattant[2][0][0] = "Donnie Yen";
+meilleur_combattant[2][0][1] = "Jet Li";
+meilleur_combattant[2][0][2] = "Jackie Chan";
+meilleur_combattant[2][1] = "Muay Thai";
+meilleur_combattant[2][1][0] = "Tony Jaa";
+
+
+
+
+
+
+
+
+
+
+
+
