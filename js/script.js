864aec2d1e233ee84d9b880f6d2230d3d81b95de
EEUAAAIA <eeuaaaia168@outlook.fr>
2022-10-22 08:14:42 -0400

    first commit

--- js/script.js
+++ js/script.js
@@ -22,35 +22,46 @@ nomDino[1] = "Rara";
 nomDino[2] = "Tritri";
 
 /*Tableaux multidimensionnels : tab dans tab*/ 
+
 var meilleur_combattant = [];
-meilleur_combattant[0] = "Muay Thai";
-meilleur_combattant[0][0] = "Saenchai";
-meilleur_combattant[0][1] = "Buakaw";
-meilleur_combattant[0][2] = "Saiyok";
-meilleur_combattant[0][3] = "Bouganhem";
-meilleur_combattant[0][4] = "Meksen";
-meilleur_combattant[1] = "Boxe anglaise";
-meilleur_combattant[1][0] = "Tyson";
-meilleur_combattant[1][1] = "Lomanchenko";
-meilleur_combattant[1][2] = "Ray Leonard";
-meilleur_combattant[1][3] = "Ali";
-meilleur_combattant[1][4] = "Mayweather";
-meilleur_combattant[2] = "Acting";
-meilleur_combattant[2][0] = "Kung-Fu";
-meilleur_combattant[2][0][0] = "Donnie Yen";
-meilleur_combattant[2][0][1] = "Jet Li";
-meilleur_combattant[2][0][2] = "Jackie Chan";
-meilleur_combattant[2][1] = "Muay Thai";
-meilleur_combattant[2][1][0] = "Tony Jaa";
+        meilleur_combattant[0] = [];
+            meilleur_combattant[0][0] = "Saenchai";
+            meilleur_combattant[0][1] = "Buakaw";
+            meilleur_combattant[0][2] = "Saiyok";
+            meilleur_combattant[0][3] = "Bouganhem";
+            meilleur_combattant[0][4] = "Meksen";
+        meilleur_combattant[1] = [];
+            meilleur_combattant[1][0] = "Tyson";
+            meilleur_combattant[1][1] = "Lomanchenko";
+            meilleur_combattant[1][2] = "Ray Leonard";
+            meilleur_combattant[1][3] = "Ali";
+            meilleur_combattant[1][4] = "Mayweather";
+        meilleur_combattant[2] = [];
+            meilleur_combattant[2][0] = [];
+                meilleur_combattant[2][0][0] = "Donnie Yen";
+                meilleur_combattant[2][0][1] = "Jet Li";
+                meilleur_combattant[2][0][2] = "Jackie Chan";
+            meilleur_combattant[2][1] = [];
+                meilleur_combattant[2][1][0] = "Tony Jaa";
 
 
+var Muay_Thai = [
+                    "Saenchai",
+                    "Buakaw",
+                    "Bouganhem",
+                    "Meksen",
+                    "Saiyok",
+                ];
 
 
+var Boxing = [
+                "Tyson",
+                "Lomanchenko",
+                "Ray Leonard",
+                "Ali",
+                "Mayweather",
+            ];
 
-
-
-
-
-
+var Fighter = [Muay_Thai,Boxing]; 
 
 
